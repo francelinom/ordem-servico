76ce29b617a0092ea5ad2b0058d2cb6084738809
Francelino <francelino.silva.prestserv@petrobras.com.br>
2023-05-18 17:09:28 -0300

    inicio component read tecnico

---
+++ src/app/views/components/tecnico/tecnico-read/tecnico-read.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tecnico-read',
+  templateUrl: './tecnico-read.component.html',
+  styleUrls: ['./tecnico-read.component.css']
+})
+export class TecnicoReadComponent {
+
+}
