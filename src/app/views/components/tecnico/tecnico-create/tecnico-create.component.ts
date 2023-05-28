676b645e8704ed393a670557177cf409e38993de
Francelino <francelino.silva.prestserv@petrobras.com.br>
2023-05-27 22:54:42 -0300

    feat create field validation

--- src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Tecnico } from 'src/app/models/tecnico';
 import { TecnicoService } from 'src/app/services/tecnico.service';
@@ -9,6 +10,16 @@ import { TecnicoService } from 'src/app/services/tecnico.service';
   styleUrls: ['./tecnico-create.component.css']
 })
 export class TecnicoCreateComponent implements OnInit {
+
+  tecnico: Tecnico = {
+    nome: '',
+    cpf: '',
+    telefone: ''
+  };
+
+  nome = new FormControl('', [Validators.minLength(5)]);
+  cpf = new FormControl('', [Validators.minLength(11)]);
+  telefone = new FormControl('', [Validators.minLength(11)]);
 
   constructor(private router: Router, private tecnicoService: TecnicoService) { }
 
@@ -21,18 +32,38 @@ export class TecnicoCreateComponent implements OnInit {
   }
 
   create(): void {
-    const tecnico: Tecnico = {
-      nome: 'Francelino',
-      cpf: '629.106.940-70',
-      telefone: '(84) 98181-2323'
-    }
-
-    this.tecnicoService.create(tecnico).subscribe(res => {
+    this.tecnicoService.create(this.tecnico).subscribe(res => {
       this.router.navigate(['/tecnicos']);
       this.tecnicoService.message('Tecnico criado com sucesso!');
     }, error => {
-      this.tecnicoService.message(error.error.error);
+      console.log(error)
+      if (error.error.error !== 'CPF já cadastrado na base de dados!' && error?.error?.erros[0].message === 'número do registro de contribuinte individual brasileiro (CPF) inválido') {
+        this.tecnicoService.message('CPF Inválido');
+      } else if (error.error.error === 'CPF já cadastrado na base de dados!') {
+        this.tecnicoService.message(error.error.error);
+      }
     })
+  }
+
+  errorValidName() {
+    if (this.nome.invalid) {
+      return 'O nome deve ter entre 5 e 100 caracteres.';
+    }
+    return false;
+  }
+
+  errorValidCpf() {
+    if (this.cpf.invalid) {
+      return 'O CPF deve ter 14 caracteres.';
+    }
+    return false;
+  }
+
+  errorValidTelefone() {
+    if (this.telefone.invalid) {
+      return 'O telefone deve ter 15 caracteres.';
+    }
+    return false;
   }
 
 }
