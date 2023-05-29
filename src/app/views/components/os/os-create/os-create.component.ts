c2f80261eadcbd907eda88c6d3be11759eea6ab6
Francelino <francelino.silva.prestserv@petrobras.com.br>
2023-05-29 15:29:43 -0300

    feat layout create os

--- src/app/views/components/os/os-create/os-create.component.ts
+++ src/app/views/components/os/os-create/os-create.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from 'src/app/models/cliente';
+import { Tecnico } from 'src/app/models/tecnico';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-os-create',
   templateUrl: './os-create.component.html',
   styleUrls: ['./os-create.component.css']
 })
-export class OsCreateComponent {
+export class OsCreateComponent implements OnInit {
 
+  selectedTecnico: any = '';
+  selectedCliente: any = '';
+  selectedStatus: any = '';
+  selectedPrioridade: any = '';
+  clientes: Cliente[] = [];
+  tecnicos: Tecnico[] = [];
+
+  constructor(
+    private tecnicoService: TecnicoService,
+    private clienteService: ClienteService) {
+
+  }
+
+  ngOnInit(): void {
+    this.listarTecnico();
+    this.listarCliente();
+  }
+
+  listarTecnico(): void {
+    this.tecnicoService.findAll().subscribe(res => {
+      this.tecnicos = res;
+    });
+  }
+
+  listarCliente(): void {
+    this.clienteService.findAll().subscribe(res => {
+      this.clientes = res;
+    });
+  }
 }
