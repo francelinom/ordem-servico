ddcffb2d0715d5c387673fc367c11b22a2d67fa6
Francelino <francelino.silva.prestserv@petrobras.com.br>
2023-05-29 12:30:27 -0300

    feat create component update cliente

---
+++ src/app/views/components/cliente/cliente-update/cliente-update.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Cliente } from 'src/app/models/cliente';
+import { ClienteService } from 'src/app/services/cliente.service';
+
+@Component({
+  selector: 'app-cliente-update',
+  templateUrl: './cliente-update.component.html',
+  styleUrls: ['./cliente-update.component.css']
+})
+export class ClienteUpdateComponent implements OnInit {
+
+  id_tec = '';
+
+  cliente: Cliente = {
+    id: '',
+    nome: '',
+    cpf: '',
+    telefone: ''
+  };
+
+  nome = new FormControl('', [Validators.minLength(5)]);
+  cpf = new FormControl('', [Validators.minLength(11)]);
+  telefone = new FormControl('', [Validators.minLength(11)]);
+
+  constructor(
+    private router: Router,
+    private clienteService: ClienteService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.id_tec = this.route.snapshot.paramMap.get('id')!;
+    this.findById();
+  }
+
+  update(): void {
+    this.clienteService.update(this.cliente).subscribe(res => {
+      this.router.navigate(['/clientes']);
+      this.clienteService.message('Cliente atualizado com sucesso.');
+    }, error => {
+      if (error?.error?.erros?.length && error?.error?.erros[0].message === 'número do registro de contribuinte individual brasileiro (CPF) inválido') {
+        this.clienteService.message('CPF Inválido');
+      } else if (error.error.error === 'CPF já cadastrado na base de dados!') {
+        this.clienteService.message(error.error.error);
+      }
+    });
+  }
+
+  findById(): void {
+    this.clienteService.findById(this.id_tec).subscribe(res => {
+      this.cliente = res;
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/clientes']);
+  }
+
+  errorValidName() {
+    if (this.nome.invalid) {
+      return 'O nome deve ter entre 5 e 100 caracteres.';
+    }
+    return false;
+  }
+
+  errorValidCpf() {
+    if (this.cpf.invalid) {
+      return 'O CPF deve ter 14 caracteres.';
+    }
+    return false;
+  }
+
+  errorValidTelefone() {
+    if (this.telefone.invalid) {
+      return 'O telefone deve ter 15 caracteres.';
+    }
+    return false;
+  }
+}
